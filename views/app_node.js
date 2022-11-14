b37e8dd822c3b4ba0edd9a3079fdb6eeb53ced59
MichalYonasi1 <112061695+MichalYonasi1@users.noreply.github.com>
2022-11-14 23:47:46 +0200

    Add files via upload

--- views/app_node.js
+++ views/app_node.js
@@ -1,11 +1,17 @@
 var http = require('http');
 var fs = require('fs'); 
+const { append } = require('vary');
+const { default: nodeTest } = require('node:test');
 var port = 8080;
+
+//app.use(express.static(path.join(__dirname, 'public')));
 
 http.createServer(function(req, res) {
     fs.readFile('main.html', function(err,data){
         res.write(data);
         return res.end();
-    });
+   });
+
+
 
 }).listen(port);
